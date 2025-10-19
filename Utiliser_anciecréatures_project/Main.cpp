85640b7cc48f6d8787038ad4556c0360b5520372
GAVLIAZ30\Lupinus <nkc23.kawai.yuto@gmail.com>
2025-10-20 00:30:58 +0900

    Add: エントリーポイントの整理

---
+++ Utiliser_anciecréatures_project/Main.cpp
@@ -0,0 +1,33 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	// 初期化前のアプリ設定
+#ifdef _DEBUG
+	ChangeWindowMode(TRUE);
+	SetOutApplicationLogValidFlag(TRUE);
+#else
+	ChangeWindowMode(FALSE);
+	SetOutApplicationLogValidFlag(FALSE);
+#endif
+
+	// ＤＸライブラリの初期化
+	if (DxLib_Init() < 0) {
+		// エラーが発生したら直ちに終了
+		return -1;
+	}
+
+	// 描画先画面を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	// Ｚバッファを有効にする
+	SetUseZBuffer3D(TRUE);
+
+	// Ｚバッファへの書き込みを有効にする
+	SetWriteZBuffer3D(TRUE);
+
+	// ＤＸライブラリの後始末
+	DxLib_End();
+
+	// ソフトの終了
+	return 0;
+}
