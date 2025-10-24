ddc6860087e57a3a73f0a9d2960bd92b091ab81b
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2025-10-24 18:51:58 +0900

    Add: マスターデータを登録するクラスを追加

---
+++ Utiliser_anciecréatures_project/src/MasterData/MasterData.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <unordered_map>
+#include <typeindex>
+#include <any>
+
+static class MasterData {
+public:
+	template<typename T>
+	static void Set(const T& data) {
+		m_masterDataMap[typeid(T)] = data;
+	}
+
+	template<typename T>
+	static const T* Get() {
+		if (!m_masterDataMap.contains(typeid(T))) {
+			return nullptr;
+		}
+
+		return std::any_cast<T>(&m_masterDataMap[typeid(T)]);
+	}
+
+private:
+	MasterData() = delete;
+	~MasterData() = delete;
+
+	static inline std::unordered_map<std::type_index, std::any> m_masterDataMap;
+};
