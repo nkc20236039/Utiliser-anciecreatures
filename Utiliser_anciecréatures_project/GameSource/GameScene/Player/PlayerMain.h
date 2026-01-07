0496d85ee62ec08bd97306518ed44c7667b892d4
Home-PC-Windows\Lupinus <nkc23.kawai.yuto@gmail.com>
2026-01-07 16:36:22 +0900

    Add: プレイヤークラスを追加

---
+++ Utiliser_anciecréatures_project/GameSource/GameScene/Player/PlayerMain.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "GameObject.h"
+
+class PlayerMain : public GameObject {
+	using GameObject::GameObject;
+
+	void Start() override {
+
+	}
+
+	void Update(const Time& time) override {
+
+	}
+};
